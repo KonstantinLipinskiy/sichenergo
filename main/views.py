78d36fdabcf5b9c4861a6f7bfbdcc5bb6b9b8dcf
KonstantinLipinskiy <nobles2007@ukr.net>
2024-12-24 23:04:18 +0200

    The main is ready, pages index

--- main/views.py
+++ main/views.py
@@ -6,11 +6,7 @@ from django.shortcuts import render
 
 def index(request):
 	context = {
-		"title": "Home", 
-		"content": "Главная страница - Сич Энерго",
-		'list': ['first', 'second'],
-		'dict': {'first': 1},
-		'is_authenticated': False
+		"title": "Головна", 
 		}
 
 	return render(request, "main/index.html", context)
