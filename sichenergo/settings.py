3bc2da04648c3edb309b922e8d1b4160386308c4
KonstantinLipinskiy <nobles2007@ukr.net>
2025-01-12 18:27:47 +0200

    add data base settings

--- sichenergo/settings.py
+++ sichenergo/settings.py
@@ -88,15 +88,21 @@ WSGI_APPLICATION = 'sichenergo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'sichenergo',
+# 		  'USER': 'sichenergo',
+# 		  'PASSWORD': 'sichenergo',
+# 		  'HOST': 'localhost',
+# 		  'PORT': '5432',
+# 		}
+# }
+
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sichenergo',
-		  'USER': 'sichenergo',
-		  'PASSWORD': 'sichenergo',
-		  'HOST': 'localhost',
-		  'PORT': '5432',
-		}
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
