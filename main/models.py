56835fd42d684b2b7797d49f0453622e67a9982c
KonstantinLipinskiy <nobles2007@ukr.net>
2025-01-18 21:31:17 +0200

    add image

--- main/models.py
+++ main/models.py
@@ -1,26 +1,30 @@
 from django.db import models
 
-class MainInfo(models.Model):
-	title = models.CharField(max_length=155, verbose_name='Головна Інформація')
-	image = models.ImageField(upload_to='main/', verbose_name='Зображення')
-	name = models.CharField(max_length=155, verbose_name='Короткий опис')
-	description = models.TextField(verbose_name='Опис')
 
-	def __str__(self):
-		return self.title
-	
-	class Meta:
-		verbose_name = 'Інформацію'
-		verbose_name_plural = 'Інформація'
+class MainInfo(models.Model):
+    title = models.CharField(max_length=155, verbose_name="Головна Інформація")
+    image = models.ImageField(upload_to="main/", verbose_name="Зображення")
+    name = models.CharField(max_length=155, verbose_name="Короткий опис")
+    description = models.TextField(verbose_name="Опис")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Інформацію"
+        verbose_name_plural = "Інформація"
+
 
 class MainItem(models.Model):
-	title = models.CharField(max_length=155, verbose_name='Головна Назва блоку')
-	image = models.ImageField(upload_to='main/', verbose_name='Зображення')
-	detail_url = models.URLField(blank=True, null=True, verbose_name='Посилання')
+    title = models.CharField(max_length=155, verbose_name="Головна Назва блоку")
+    image = models.ImageField(
+        upload_to="main/", null=True, blank=True, verbose_name="Зображення"
+    )
+    detail_url = models.URLField(blank=True, null=True, verbose_name="Посилання")
 
-	def __str__(self):
-		return self.title
-	
-	class Meta:
-		verbose_name = 'Блок'
-		verbose_name_plural = 'Блок'
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Блок"
+        verbose_name_plural = "Блок"
