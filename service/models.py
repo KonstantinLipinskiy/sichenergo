af8a7743d4acd2a155f20d51f61c7cbcbc0a2305
KonstantinLipinskiy <nobles2007@ukr.net>
2025-01-20 21:14:04 +0200

    add service video

--- service/models.py
+++ service/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from cloudinary_storage.storage import RawMediaCloudinaryStorage
 
 class ServiceInform(models.Model):
 	title = models.CharField(max_length=155, verbose_name='Інформація', blank=True, null=True)
-	video = models.FileField(upload_to='video/', verbose_name='Відео', blank=True, null=True)
+	video = models.FileField(upload_to='videos/', blank=True, null=True, storage=RawMediaCloudinaryStorage(), verbose_name='Відео')
 	name = models.CharField(max_length=155, verbose_name='Назва події', blank=True, null=True)
 	description = models.TextField(verbose_name='Опис', blank=True, null=True)
 
