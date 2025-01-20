ac7b134fce7374631002680ce3739d125b0eb5dc
KonstantinLipinskiy <nobles2007@ukr.net>
2025-01-20 19:15:39 +0200

    add raw

--- products/models.py
+++ products/models.py
@@ -42,7 +42,7 @@ class TransformTm(models.Model):
     image = CloudinaryField(
         default="static/deps/img/trans.png", verbose_name="Зображення"
     )
-    file_tm = CloudinaryField(resource_type='auto', verbose_name="Скачати креслення трансформаторів ТМ та ТМЖ" )
+    file_tm = CloudinaryField(resource_type='raw', verbose_name="Скачати креслення трансформаторів ТМ та ТМЖ" )
 
     def __str__(self):
         return self.name
@@ -59,7 +59,7 @@ class TransformTmg(models.Model):
     image = CloudinaryField(
         default="static/deps/img/trans.png", verbose_name="Зображення"
     )
-    file_tmg = CloudinaryField(resource_type='auto', verbose_name="Скачати креслення трансформаторів ТМГ та ТМЖГ" )
+    file_tmg = CloudinaryField(resource_type='raw', verbose_name="Скачати креслення трансформаторів ТМГ та ТМЖГ" )
 
     def __str__(self):
         return self.name
@@ -74,7 +74,7 @@ class TransformTmz(models.Model):
     image = CloudinaryField(
         default="static/deps/img/trans.png", verbose_name="Зображення"
     )
-    file_tmz = CloudinaryField(resource_type='auto', verbose_name="Скачати креслення трансформаторів ТМЗ" )
+    file_tmz = CloudinaryField(resource_type='raw', verbose_name="Скачати креслення трансформаторів ТМЗ" )
 
     def __str__(self):
         return self.name
@@ -89,7 +89,7 @@ class TransformYZ(models.Model):
     image = CloudinaryField(
         default="static/deps/img/trans.png", verbose_name="Зображення"
     )
-    file_yz = CloudinaryField(resource_type='auto', verbose_name="Скачати креслення трансформаторів Y/Z" )
+    file_yz = CloudinaryField(resource_type='raw', verbose_name="Скачати креслення трансформаторів Y/Z" )
 
     def __str__(self):
         return self.name
